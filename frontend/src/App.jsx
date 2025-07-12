d7d9417da3ec521ff34e1fd1b780bb42304be352
Xtractyl <duecker@bonsign.com>
2025-07-12 16:38:36 +0200

    Frontend fixes im Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Compare from './pages/Compare';
+import Navigation from './components/Navigation';
+
 
 export default function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/compare" element={<Compare />} />
