da3a093ab2867bba7390363e42087f0cff2b5293
Xtractyl <duecker@bonsign.com>
2025-08-10 14:19:40 +0200

    FinetuneAIPage component refactored
    currently it has no hooks and api, but later on it will

---
+++ frontend/src/components/FinetuneAIPage/FinetuneAICard.jsx
@@ -0,0 +1,11 @@
+export default function FinetuneAICard() {
+    return (
+      <div>
+        <h1 className="text-2xl font-semibold mb-4">Finetune AI</h1>
+        <p className="text-gray-600">
+          This feature will be included in later releases. It will allow you
+          to finetune a model based on your own data reviewed in Label Studio.
+        </p>
+      </div>
+    );
+  }
--- frontend/src/pages/FinetuneAI.jsx
+++ frontend/src/pages/FinetuneAI.jsx
@@ -1,8 +1,5 @@
+import FinetuneAICard from "../components/FinetuneAIPage/FinetuneAICard";
+
 export default function FinetuneAIPage() {
-    return (
-      <div>
-        <h1 className="text-2xl font-semibold mb-4">Finetune AI</h1>
-        <p className="text-gray-600">This feature will be included in later releases. It will allow to finetune a model based on the own data reviewed in Label Studio.</p>
-      </div>
-    )
-  }
+  return <FinetuneAICard />;
+}
