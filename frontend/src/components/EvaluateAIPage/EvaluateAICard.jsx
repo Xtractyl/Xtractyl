7fde141710b6ba9107810f7630a51d43a04ddb01
Xtractyl <duecker@bonsign.com>
2025-08-10 14:17:23 +0200

    EvaluateAIPage component refactored
    Currently has no hooks and api, but later on it will need them

---
+++ frontend/src/components/EvaluateAIPage/EvaluateAICard.jsx
@@ -0,0 +1,12 @@
+export default function EvaluateAICard() {
+    return (
+      <div>
+        <h1 className="text-2xl font-semibold mb-4">Evaluate AI</h1>
+        <p className="text-gray-600">
+          This feature will be included in later releases. It will allow you
+          to compare a model against others using metrics like precision,
+          recall, and F1.
+        </p>
+      </div>
+    );
+  }
--- frontend/src/pages/EvaluateAI.jsx
+++ frontend/src/pages/EvaluateAI.jsx
@@ -1,8 +1,5 @@
+import EvaluateAICard from "../components/EvaluateAIPage/EvaluateAICard";
+
 export default function EvaluateAIPage() {
-    return (
-      <div>
-        <h1 className="text-2xl font-semibold mb-4">Evaluate AI</h1>
-        <p className="text-gray-600">This feature will be included in later releases. It will allow to compare a model against others by metrics like precision, recall, F1.</p>
-      </div>
-    )
-  }
+  return <EvaluateAICard />;
+}
