5c408939802d45343add58e6aa1e2d345780f26b
Xtractyl <duecker@bonsign.com>
2025-12-13 20:53:40 +0100

    Test if lintering is reactivated

--- frontend/src/components/AskQuestionPage/AskQuestionCard.jsx
+++ frontend/src/components/AskQuestionPage/AskQuestionCard.jsx
@@ -10,7 +10,7 @@ export default function AskQuestionCard({ apiToken }) {
     <div className="p-6 bg-[#e6e2cf] min-h-screen text-[#23211c]">
       <h1 className="text-2xl font-semibold mb-4">Under Construction</h1>
       <p className="text-gray-600 mb-6">
-        This feature is under construction.
+        This feature is under construction
       </p>
 
     </div>
