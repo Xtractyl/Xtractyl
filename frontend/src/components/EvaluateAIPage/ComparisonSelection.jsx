b9d845480de31d4ffc7e2bba0de4d792670b97f8
Xtractyl <duecker@bonsign.com>
2025-12-28 18:10:00 +0100

    Add information that evaluation is saved as JSON

--- frontend/src/components/EvaluateAIPage/ComparisonSelection.jsx
+++ frontend/src/components/EvaluateAIPage/ComparisonSelection.jsx
@@ -77,7 +77,7 @@ export default function ComparisonSelection({
             className="mt-4 inline-flex items-center justify-center px-4 py-2 rounded bg-[#6baa56] text-white text-sm font-medium shadow hover:bg-green-700 transition disabled:opacity-60 disabled:cursor-not-allowed"
             disabled={!groundtruthProject || !comparisonProject}
           >
-            Run Evaluation
+            Run Evaluation and Save as JSON
           </button>
         </>
       )}
