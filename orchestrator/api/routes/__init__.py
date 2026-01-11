23c6698e0a20252d27ca4e788d2b361667f50870
Xtractyl <duecker@bonsign.com>
2026-01-11 11:56:36 +0100

    XC-136: Create API route registry

---
+++ orchestrator/api/routes/__init__.py
@@ -0,0 +1,8 @@
+# orchestrator/api/routes/__init__.py
+
+def register_routes(app):
+    """
+    Central registry for API routes.
+    Route modules will be registered incrementally.
+    """
+    return
