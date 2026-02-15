d0599bce0d534c88c2809fe38b26ec44a88987be
Xtractyl <duecker@bonsign.com>
2026-02-15 13:52:48 +0100

    Correct typo

--- frontend/src/components/EvaluationDriftPage/EvaluationDriftCard.jsx
+++ frontend/src/components/EvaluationDriftPage/EvaluationDriftCard.jsx
@@ -5,7 +5,7 @@ export default function EvaluationDriftCard() {
         <div className="p-8 bg-xtractyl-background min-h-screen text-xtractyl-darktext">
         <h1 className="text-2xl font-semibold mb-4">Evaluation Drift</h1>
         <p className="text-xtractyl-outline/70">
-          This feature is currently worked on. It will display evaluation dirft for a standard dataset over time.
+          This feature is currently worked on. It will display evaluation drift for a standard dataset over time.
         </p>
       </div>
     );
