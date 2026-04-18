0c3c877f33ccb0abf57ab291cf3041477eb83648
Xtractyl <duecker@bonsign.com>
2026-04-18 18:05:07 +0200

    Add missing path information for remaining .jsx files in src

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+//src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/shared/Layout';
 import UploadandConversionPage from './pages/PDFUploadandConversion.jsx';
