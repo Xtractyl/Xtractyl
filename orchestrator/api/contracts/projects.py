44ee2c9f0ab0b53c41f44948f7b4bfcb3719a272
Xtractyl <duecker@bonsign.com>
2026-07-25 08:58:45 +0200

    Fix: Add response contract for route /list_projects_without_label_studio_id to guide user creating label studio projects only for projects set up in xtractyl first (project_exist check has been implemented in /create_project route/domain already to secure against unintended creation of label studio projects not available in xtractyl)

--- orchestrator/api/contracts/projects.py
+++ orchestrator/api/contracts/projects.py
@@ -31,6 +31,10 @@ class ListProjectsReadyForUploadResponse(BaseModel):
     projects: list[str]
 
 
+class ListProjectsWithoutLabelStudioIdResponse(BaseModel):
+    projects: list[str]
+
+
 class PreviewQalResponse(BaseModel):
     data: Any
 
